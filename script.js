2fd5929f963158cab8c17b4c6d69f5bfed9bf6e5
Dipak <dipakxb@gmail.com>
2026-03-26 19:28:50 +0530

    add operate on equals
    operate with the operands when = is pressed (buggy)

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ let operandTwo = '';
 
 //operate takes 2 numbers and operator then calls the right function 
 const operate = function(operandOne, sym, operandTwo){
-    const operators = [["+", "add"], ["-", "subtract"], ["/", "divide"], ["*", "multiply"], ["**", "power"], ["!", "factorial"]];
+    const operators = [["+", "add"], ["-", "subtract"], ["÷", "divide"], ["×", "multiply"], ["**", "power"], ["!", "factorial"]];
     for(let operator of operators){
         if(operator[0] == sym){
-            return window[operator[1]](operandOne, operandTwo)
+            return window[operator[1]](parseFloat(operandOne), parseFloat(operandTwo))
         }
     }
     return
@@ -46,7 +46,7 @@ const operate = function(operandOne, sym, operandTwo){
 //keys for calculators
 const keys = document.querySelector('.keys');
 const keyArr = [['C','clear'], ['%', 'percent'], ["←", 'back'],
-                ['÷', 'divide'], 7, 8, 9, ['x', 'multiply'], 4, 5, 6, ['-', 'subtract'], 1, 2, 3, ['+', 'add'],
+                ['÷', 'divide'], 7, 8, 9, ['×', 'multiply'], 4, 5, 6, ['-', 'subtract'], 1, 2, 3, ['+', 'add'],
                 ['00', '00' ], 0, ['.', 'decimal'], ['=', 'equal']];
 for(let key of keyArr){
     let button = document.createElement('div');
@@ -61,25 +61,36 @@ for(let key of keyArr){
 }
 
 let isOperatorPressed = false;
-//function to update one of the number variable
+//function to update number variable
 keys.addEventListener('click', (e)=> {
     const nums = ['1','2','3','4','5','6','7','8','9','0','00'];
-    const operatorArr = ['+', '-', 'x', '/', '%'];
+    const operatorArr = ['+', '-', '×', '÷', '%', '='];
     const keyValue = e.target.innerText;
     let display = document.querySelector('.display');
 
     if(operatorArr.includes(keyValue)){
-        isOperatorPressed = true;
-        operator = keyValue;
+        if(isOperatorPressed){
+            if(operandTwo.length > 0){
+                operandOne = operate(operandOne, operator, operandTwo);
+                operandTwo = '';
+                keyValue == '=' ? isOperatorPressed = false : operator = keyValue;
+            }
+            display.innerHTML = operandOne;
+        }
+        if(operandOne.length > 0){
+            isOperatorPressed = true;
+            operator = keyValue;
+        }
+        keyValue == '=' ? false : display.innerHTML += keyValue;
     }
     
     if(nums.includes(keyValue)){
         if(isOperatorPressed){
             operandTwo += keyValue;
+            display.innerHTML += keyValue;
         }else {
             operandOne += keyValue;
+            display.innerHTML += keyValue;
         }
     }
-
-    display.innerHTML += keyValue;
 })
